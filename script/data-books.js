e2c78a837b6dfe39cc1d1e03c64248cbc17590e2
FredMoriam <rimandofredrik@gmail.com>
2025-12-01 12:20:14 +0800

    refactor: move data into separate files
    - books and movies

---
+++ script/data-books.js
@@ -0,0 +1,142 @@
+const BOOKS = [
+  {
+    "title": "Infinite Jest",
+    "tag": "Philosophical Fiction",
+    "author": "David Foster Wallace",
+    "year": 1996,
+    "coverFilename": "infinite-jest.jpg"
+  },
+  {
+    "title": "The Stranger",
+    "tag": "Existentialism",
+    "author": "Albert Camus",
+    "year": 1942,
+    "coverFilename": "the-stranger.jpg"
+  },
+  {
+    "title": "Thus Spoke Zarathustra",
+    "tag": "Philosophy",
+    "author": "Friedrich Nietzsche",
+    "year": 1883,
+    "coverFilename": "thus-spoke-zarathustra.jpg"
+  },
+  {
+    "title": "Meditations",
+    "tag": "Stoic Philosophy",
+    "author": "Marcus Aurelius",
+    "year": 2003,
+    "coverFilename": "meditations.jpg"
+  },
+  {
+    "title": "The Brothers Karamazov",
+    "tag": "Classic Literature",
+    "author": "Fyodor Dostoevsky",
+    "year": 1880,
+    "coverFilename": "the-brothers-karamazov.jpg"
+  },
+  {
+    "title": "Blood Meridian",
+    "tag": "Gritty Literature",
+    "author": "Cormac McCarthy",
+    "year": 1985,
+    "coverFilename": "blood-meridian.jpg"
+  },
+  {
+    "title": "The Road",
+    "tag": "Post-Apocalyptic Fiction",
+    "author": "Cormac McCarthy",
+    "year": 2006,
+    "coverFilename": "the-road.jpg"
+  },
+  {
+    "title": "American Psycho",
+    "tag": "Satire / Hyper-Masculine Critique",
+    "author": "Bret Easton Ellis",
+    "year": 1991,
+    "coverFilename": "american-psycho.png"
+  },
+  {
+    "title": "Fight Club",
+    "tag": "Dark Satire",
+    "author": "Chuck Palahniuk",
+    "year": 1996,
+    "coverFilename": ""
+  },
+  {
+    "title": "The Old Man and the Sea",
+    "tag": "Classic Masculinity",
+    "author": "Ernest Hemingway",
+    "year": 1952,
+    "coverFilename": ""
+  },
+  {
+    "title": "The 48 Laws of Power",
+    "tag": "Self-Help / Strategy",
+    "author": "Robert Greene",
+    "year": 1998,
+    "coverFilename": ""
+  },
+  {
+    "title": "Atomic Habits",
+    "tag": "Self-Improvement",
+    "author": "James Clear",
+    "year": 2018,
+    "coverFilename": ""
+  },
+  {
+    "title": "Can't Hurt Me",
+    "tag": "Grit / Self-Help",
+    "author": "David Goggins",
+    "year": 2018,
+    "coverFilename": ""
+  },
+  {
+    "title": "The Art of War",
+    "tag": "Strategy",
+    "author": "Sun Tzu",
+    "year": 1963,
+    "coverFilename": ""
+  },
+  {
+    "title": "Sapiens",
+    "tag": "Pop Nonfiction / Anthropology Flex",
+    "author": "Yuval Noah Harari",
+    "year": 2014,
+    "coverFilename": ""
+  },
+  {
+    "title": "Zero to One",
+    "tag": "Startup / Business",
+    "author": "Peter Thiel",
+    "year": 2014,
+    "coverFilename": ""
+  },
+  {
+    "title": "Gödel, Escher, Bach",
+    "tag": "Intellectual Flex / Math-Philosophy",
+    "author": "Douglas Hofstadter",
+    "year": 1979,
+    "coverFilename": ""
+  },
+  {
+    "title": "1984",
+    "tag": "Classic Dystopia",
+    "author": "George Orwell",
+    "year": 1949,
+    "coverFilename": ""
+  },
+  {
+    "title": "On the Road",
+    "tag": "Beat Literature",
+    "author": "Jack Kerouac",
+    "year": 1957,
+    "coverFilename": ""
+  },
+  {
+    "title": "Slaughterhouse-Five",
+    "tag": "Anti-War Literature",
+    "author": "Kurt Vonnegut",
+    "year": 1969,
+    "coverFilename": ""
+  }
+];
---
+++ script/data-movies.js
@@ -0,0 +1,74 @@
+const MOVIES = [
+    {
+        "title": "Good Will Hunting",
+        "posterFilename": "good-will-hunting.jpg"
+    },
+    {
+        "title": "Aftersun",
+        "posterFilename": "aftersun.jpg"
+    },
+    {
+        "title": "Dead Poet's Society",
+        "posterFilename": "dead-poets-society.jpg"
+    },
+    {
+        "title": "Moonlight",
+        "posterFilename": "moonlight.jpg"
+    },
+    {
+        "title": "Perks of Being a Wallflower",
+        "posterFilename": "perks-of-being-a-wallflower.jpg"
+    },
+    {
+        "title": "12 Angry Men",
+        "posterFilename": "12-angry-men.jpg"
+    },
+    {
+        "title": "Memories of Murder",
+        "posterFilename": "memories-of-murder.jpg"
+    },
+    {
+        "title": "Harakiri",
+        "posterFilename": "harakiri.jpg"
+    },
+    {
+        "title": "Come and See",
+        "posterFilename": "come-and-see.jpg"
+    },
+    {
+        "title": "City of God",
+        "posterFilename": "city-of-god.jpg"
+    },
+    {
+        "title": "Oldboy",
+        "posterFilename": "oldboy.jpg"
+    },
+    {
+        "title": "Gitling",
+        "posterFilename": "gitling.jpg"
+    },
+    {
+        "title": "I'm Drunk I Love You",
+        "posterFilename": "im-drunk-i-love-you.jpg"
+    },
+    {
+        "title": "Anatomy Of A Fall",
+        "posterFilename": "anatomy-of-a-fall.jpg"
+    },
+    {
+        "title": "Tanabata's Wife",
+        "posterFilename": "tanabatas-wife.jpg"
+    },
+    {
+        "title": "Drive My Car",
+        "posterFilename": "drive-my-car.jpg"
+    },
+    {
+        "title": "Decision To Leave",
+        "posterFilename": "decision-to-leave.jpg"
+    },
+    {
+        "title": "Fallen Angels",
+        "posterFilename": "fallen-angels.jpg"
+    },
+];
